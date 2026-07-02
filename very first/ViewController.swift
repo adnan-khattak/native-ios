0ef445c40409cf10d33929cb7b666074daa859bf
Adnan Khattak <adnanktk540@gmail.com>
2026-07-02 11:50:52 +0500

    feat: section code

---
+++ very first/ViewController.swift
@@ -0,0 +1,20 @@
+ //
+//  ViewController.swift
+//  very first
+//
+//  Created by MacbookPro 14" 2020 on 01/07/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var titleUpdate: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titleUpdate.text = "design link"
+    }
+
+
+}
+
